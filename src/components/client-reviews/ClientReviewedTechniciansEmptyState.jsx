a9845d592a15ee6b65424af0c2633ed4610b0662
JuanJose-96 <jjsaVIXXXIII@gmail.com>
2026-05-10 20:36:17 +0200

    Creacion de los components de ClientReview

---
+++ src/components/client-reviews/ClientReviewedTechniciansEmptyState.jsx
@@ -0,0 +1,16 @@
+function ClientReviewedTechniciansEmptyState() {
+    return (
+        <section className="client-reviewed-technicians-empty">
+            <h2 className="client-reviewed-technicians-empty__title">
+                Todavía no has publicado reseñas
+            </h2>
+
+            <p className="client-reviewed-technicians-empty__description">
+                Cuando reseñes a uno o varios técnicos, aparecerán aquí ordenados por la
+                valoración que les hayas dado.
+            </p>
+        </section>
+    )
+}
+
+export default ClientReviewedTechniciansEmptyState
