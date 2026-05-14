7f725bea6c53e3350d1b0c1a304788ad8196e28c
JuanJose-96 <jjsaVIXXXIII@gmail.com>
2026-05-14 16:51:14 +0200

    Services de la busqueda de tecnico y cliente

--- src/services/clientSearchService.js
+++ src/services/clientSearchService.js
@@ -4,7 +4,7 @@ export async function searchClients(filters = {}) {
     const params = {}
 
     if (filters.name) {
-        params.name = filters.name.trim().toLowerCase() + '%'
+        params.name = filters.name.trim()
     }
 
     if (filters.province) {
@@ -15,6 +15,18 @@ export async function searchClients(filters = {}) {
         params.city = filters.city
     }
 
+    if (typeof filters.page === 'number') {
+        params.page = filters.page
+    }
+
     const response = await axiosClient.get('/clients/search', { params })
-    return response.data
+    const data = response.data
+
+    return {
+        content: Array.isArray(data?.content) ? data.content : [],
+        currentPage: typeof data?.currentPage === 'number' ? data.currentPage : 0,
+        totalPages: typeof data?.totalPages === 'number' ? data.totalPages : 0,
+        totalElements: typeof data?.totalElements === 'number' ? data.totalElements : 0,
+        hasNext: Boolean(data?.hasNext),
+    }
 }
